fbecb51fd4cb9b2c154907cd555f60a7c4202b57
Essau Ramirez <sauramirez@users.noreply.github.com>
2023-02-20 12:41:01 -0600

    Complete oauth flow

--- lib/auth/oauth.dart
+++ lib/auth/oauth.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_home_connect_sdk/src/auth.dart';
 import '../components/login.dart' show showLogin;
@@ -21,9 +24,21 @@ class HomeConnectOauth extends HomeConnectAuth {
     if (response == null) {
       throw Exception("Login failed");
     }
+    final tokenResponse = await http.post(
+      Uri.parse('https://simulator.home-connect.com/security/oauth/token'),
+      body: {
+        'grant_type': 'authorization_code',
+        'code': response['token'],
+        'client_id': credentials.clientId,
+        'redirect_uri': credentials.redirectUri,
+      },
+    );
+    final res = jsonDecode(utf8.decode(tokenResponse.bodyBytes)) as Map;
+    print("body ${res}");
+    //final accessToken = tokenResponse.body['accessToken'];
     return HomeConnectAuthCredentials(
-      accessToken: "",
-      refreshToken: "",
+      accessToken: res['accessToken'],
+      refreshToken: res['refreshToken'],
       //expirationDate: DateTime.now().add(const Duration(days: 1)),
     );
   }
